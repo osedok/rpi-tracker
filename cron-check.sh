d75ce019aa7ce47fd7b0081a4c3074b849446022
zero <zero@null.(none)>
2014-07-25 20:33:26 -0500

    changed location of script.

--- cron-check.sh
+++ cron-check.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 #
+#This script exists to check if rpi-tracker is running in order to prevent
+#cron from executing the rpi-tracker.sh script concurrently
+#
 if pidof -x "rpi-tracker.sh"; then
-   exit
+      exit 0
  else
-     if pidof ptunnel; then
-        killall ptunnel
-      else
-        /bin/sh -c /root/rpi-tracker/rpi-tracker.sh
-        exit
-    fi
+      if pidof ptunnel; then
+           killall ptunnel
+       else
+          /bin/sh -c /root/tracking-engine/rpi-tracker.sh
+          exit 0
+      fi
 fi
+
 exit
